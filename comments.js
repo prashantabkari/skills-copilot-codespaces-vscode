f6bd6cd56612931e7893b88589db919dac091efb
prashantabkari <prashantabkari@gmail.com>
2024-10-10 01:01:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,18 @@
 // create web server
-const http = require('http');
+// create a route for comments
+// create a route for comments/:commentId
 
-// create server
-http.createServer((req, res) => {
-    // send response
-    res.end('Hello World');
-}).listen(3000, () => console.log('Server is running...'));
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.send('This is the comments route');
+});
+
+app.get('/comments/:commentId', (req, res) => {
+  res.send(`This is the comment with id ${req.params.commentId}`);
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
